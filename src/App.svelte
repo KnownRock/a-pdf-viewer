47479e7ccc99fda4e8070089dacd2afa1070992c
KnownRock <knownrock@hotmail.com>
2023-04-21 15:18:46 +0800

    clear code

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  // import BookShelf from './lib/BookShelf.svelte'
-  // import Resume from './lib/Resume.svelte'
   import Loading from './lib/Loading.svelte'
-  // import PermissionCheck from './lib/PermissionCheck.svelte'
   import { isLoading } from 'svelte-i18n'
 
   import { simpleFs as simpleFsStore } from './store'
-  // import Viewer from './lib/Viewer.svelte'
   import { SimpleFsName, type SimpleFs } from './types'
 
   let simpleFs: SimpleFs | null = null
@@ -14,18 +10,14 @@
     simpleFs = value.simpleFs
   })
 
-
   import { Router, Route } from 'svelte-routing'
   import { onMount } from 'svelte'
   import { getSimpleFs } from './utils/simple-fs'
   import Message from './lib/Message.svelte'
   import Delay from './lib/general/Delay.svelte'
-  // import { getSimpleFsName } from './utils/config'
 
   onMount(() => {
     async function main () {
-      // const simpleFsName = await getSimpleFsName() ?? SimpleFsName.idb
-      // TODO: finish fs api mode
       const simpleFsName = SimpleFsName.idb
       const simpleFs = await getSimpleFs(simpleFsName)
       simpleFsStore.set({ simpleFs })
@@ -43,8 +35,6 @@
     <Loading isStaticallyLoading={true} />
   </Delay>
 {:else}
-  <!-- <Resume /> -->
-  <!-- <PermissionCheck /> -->
   <Message />
   <Loading />
 
@@ -52,7 +42,7 @@
   <Router url="{url}">
     <Route path="/book/:id" let:params >
       {#await import('./lib/Viewer.svelte').then((module) => module.default)}
-        loading
+        <Loading isStaticallyLoading={true} />
       {:then Viewer}
         <Viewer simpleFs={simpleFs} bookId={params.id} />
       {/await}
@@ -60,7 +50,7 @@
 
     <Route path="/*" >
       {#await import('./lib/BookShelf.svelte').then((module) => module.default)}
-        loading
+        <Loading isStaticallyLoading={true} />
       {:then BookShelf}
         <BookShelf simpleFs={simpleFs}  />
       {/await}
