18294861f627a1a99436c536ecaa55810809507e
KnownRock <knownrock@hotmail.com>
2023-04-21 09:10:56 +0800

    s3 setting

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 import { set, get } from 'idb-keyval'
-import type { SimpleFsName } from '../types'
+import type { S3Config, SimpleFsName } from '../types'
 
 export async function setSimpleFsName (name: SimpleFsName): Promise<void> {
   await set('app:simple-fs', name)
@@ -7,4 +7,20 @@ export async function setSimpleFsName (name: SimpleFsName): Promise<void> {
 
 export async function getSimpleFsName (): Promise<SimpleFsName> {
   return await get('app:simple-fs') as SimpleFsName
+}
+
+export async function setS3Config (options: S3Config): Promise<void> {
+  await set('app:s3', options)
+}
+
+export async function getS3Config (): Promise<S3Config> {
+  const result = await get('app:s3')
+  return {
+    accessKeyId: result?.accessKeyId ?? '',
+    secretAccessKey: result.secretAccessKey ?? '',
+    region: result.region ?? '',
+    endpoint: result.endpoint ?? '',
+    bucket: result.bucket ?? '',
+    prefix: result.prefix ?? ''
+  }
 }
